7e3919396566bd555a0063befa7c31e39fcb2fa7
Mayer MICHEL <mayer.mohib@gmail.com>
2024-12-09 09:10:26 +0100

    nettoyage des log et de vendor + creation du controller

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,27 @@
 <?php
 
-phpinfo();
+// Chargement du système d'autoload
+require_once '../vendor/autoload.php';
+
+use App\Controller\PageController;
+use MiladRahimi\PhpRouter\Exceptions\RouteNotFoundException;
+use MiladRahimi\PhpRouter\Router;
+
+$router = Router::create();
+
+$router->get( '/', [ PageController::class, 'index' ] );
+$router->get( '/mentions-legales', [ PageController::class, 'legalNotice' ]);
+
+try{
+    $router->dispatch();
+}
+// Page 404 avec status HTTP adequat pour les pages non listée dans le routeur
+catch( RouteNotFoundException $e ) {
+    http_response_code( 404 );
+    echo 'Oups... La page n\'existe pas';
+}
+// Erreur 500 avec status HTTP adequat pour tout autre problème temporaire ou non
+catch( Throwable $e ) {
+    http_response_code( 500 );
+    echo 'Erreur interne du serveur';
+}
---
+++ app/src/Controller/PageController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+class PageController
+{
+    // Page d'accueil
+    public function index(): void
+    {
+        echo 'Bonjour depuis le Controller';
+    }
+    // Page mentions légales
+    public function legalNotice(): void
+    {
+        echo 'Les mentions légales !';
+    }
+}
