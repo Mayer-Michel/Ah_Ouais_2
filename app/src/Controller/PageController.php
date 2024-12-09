edfed32f8e0bde767b9f527bd49e92e210772498
Mayer MICHEL <mayer.mohib@gmail.com>
2024-12-09 09:36:13 +0100

    MVC: incomplet

--- app/src/Controller/PageController.php
+++ app/src/Controller/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Symplefony\View;
+
 class PageController
 {
     // Page d'accueil
     public function index(): void
     {
-        $view = new View();
+        $view = new View( 'page:home' );
+
         $view->render();
     }
 
